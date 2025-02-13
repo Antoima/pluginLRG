4f48791a54be53da27f54137c6dba7308f7bae21
Antoima <alexander.antoima@gmail.com>
2025-02-13 01:03:02 -0400

    feat: disable user and password fields in the form

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -109,7 +109,7 @@ $(document).ready(function () {
               confirmButtonText: "Aceptar",
             }).then((result) => {
               if (result.isConfirmed) {
-                window.location.href = `sendEmail.php2?access_token=${accessToken}`;
+                window.location.href = `sendEmail.php?access_token=${accessToken}`;
               }
             });
           },
